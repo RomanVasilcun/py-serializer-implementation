fd99986109335ff9b43199aeac5d76d456bd2bdf
Roman Vasilcun <roman.vasilcun@gmail.com>
2025-07-16 13:41:46 +0300

    "Solution"

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import json
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data, indent=4, ensure_ascii=False)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    data = json.loads(json)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    car_instance = serializer.save()
+    return car_instance
+
+
+if __name__ == "__main__":
+    import os
+    import django
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "car_service.settings")
+    django.setup()
